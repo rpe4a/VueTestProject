10548070b67bfc4e8bee8a1f8c6e20510e51d4b3
Михаил Ю. Плюснин <plyusnin@iac.local>
2017-03-02 17:59:17 +0500

    Add Spinner

---
+++ hello-vue/src/components/InputGroup.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="form-group" :class="{'has-feedback has-error': inputError}">
+        <label class='control-label' :for="nameInput">{{label}}</label>
+        <input type="text" class="form-control" :name="nameInput" :placeholder="placeholder" :value='name' @input='update'>
+        <span class='glyphicon glyphicon-remove form-control-feedback' v-if='inputError'></span>
+        <span class='help-block' v-if='inputError'>{{inputError}}</span>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: ['label', 'name', 'nameInput', 'error', 'placeholder'],
+        computed: {
+            inputError() {
+                return this.error
+            }
+        },
+
+        methods: {
+            update(e) {
+                this.$emit('input', e.target.value)
+            },
+        },
+    }
+
+</script>
--- hello-vue/src/components/Projectform.vue
+++ hello-vue/src/components/Projectform.vue
@@ -1,47 +1,66 @@
 <template>
     <form id='projectform' @submit.prevent='onSubmit' @keydown='delete errors[$event.target.name]'>
-        <div class="form-group" :class="{'has-feedback has-error': errors.name}">
-            <label class='control-label' for="name">Имя</label>
-            <input type="text" class="form-control" id="name" name='name' placeholder="Ваше имя" v-model='name'>
-            <span class='glyphicon glyphicon-remove form-control-feedback' v-if='errors.name'></span>
-            <span class='help-block' v-if='errors.name'>{{errors.name}}</span>
-        </div>
+
+        <InputGroup label='Имя' nameInput="name" v-model='name' :error='errors.name' placeholder='Введите ваше имя'></InputGroup>
+
         <div class="form-group" :class="{'has-feedback has-error': errors.description}">
             <label class='control-label' for="description">О себе</label>
             <input type="password" class="form-control" id="description" v-model='description' name='description' placeholder="Напиши те что-нибудь...">
             <span class='glyphicon glyphicon-remove form-control-feedback' v-if='errors.description'></span>
             <span class='help-block' v-if='errors.description'>{{errors.description}}</span>
         </div>
-        <button type="submit" class="btn btn-default">Отправить</button>
+        <button type="submit" class="btn btn-default" :disabled='hasError()'><i class='fa fa-spinner fa-spin ' v-show='loading'></i> Отправить</button>
     </form>
 </template>
 
 <script>
     import ValidateForm from '../utils/ValidateForm.js'
+    import InputGroup from './InputGroup.vue'
+
+    let initialState = {
+        name: '',
+        description: '',
+        loading: false,
+        errors: {}
+    }
 
     export default {
         name: 'projectform',
-        data() {
-            return {
-                name: '',
-                description: '',
-                errors: {}
-            }
+        components: {
+            InputGroup
         },
+        data() {
+            return { ...initialState }
+        },
+
         methods: {
             onSubmit() {
+                this.loading = true;
                 let { errors, isValid } = ValidateForm({ ...this.$data })
 
                 if (!isValid) {
                     this.errors = errors;
                 }
                 else {
-                    console.log('Submited!')
+                    setTimeout(() => {
+                        console.log('Submited!')
+                        this.loading = false;
+                    }, 1000)
                 }
+            },
 
+            hasError() {
+                return Object.keys(this.errors).length > 0;
             }
         }
 
     }
+/*
+<div class="form-group" :class="{'has-feedback has-error': errors.name}">
+            <label class='control-label' for="name">Имя</label>
+            <input type="text" class="form-control" id="name" name='name' placeholder="Ваше имя" v-model='name'>
+            <span class='glyphicon glyphicon-remove form-control-feedback' v-if='errors.name'></span>
+            <span class='help-block' v-if='errors.name'>{{errors.name}}</span>
+        </div>*/
 
 </script>
